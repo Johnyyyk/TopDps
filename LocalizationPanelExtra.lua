7a076c6638cd5cf129b9d1769ba13f1ef2fef775
Johnyyyk <36297935+Johnyyyk@users.noreply.github.com>
2026-08-11 20:09:39 +0300

    Добавить названия инженерных источников панели

--- LocalizationPanelExtra.lua
+++ LocalizationPanelExtra.lua
@@ -3,13 +3,21 @@ local L = addon.L
 
 if GetLocale() == "ruRU" then
     L.COOLDOWN_PANEL_ICONS_PER_ROW = "Иконок в строке: %d"
+    L.COOLDOWN_EQUIPMENT_HEAD_ENCHANT = "Чары головы / инженерное улучшение"
     L.COOLDOWN_EQUIPMENT_CLOAK_ENCHANT = "Чары плаща"
+    L.COOLDOWN_EQUIPMENT_HANDS_ENCHANT = "Чары перчаток / инженерное улучшение"
+    L.COOLDOWN_EQUIPMENT_WAIST_ENCHANT = "Инженерное улучшение пояса"
+    L.COOLDOWN_EQUIPMENT_FEET_ENCHANT = "Чары ботинок / инженерное улучшение"
     L.COOLDOWN_EQUIPMENT_MAINHAND_ENCHANT = "Чары основной руки"
     L.COOLDOWN_EQUIPMENT_OFFHAND_ENCHANT = "Чары второй руки"
     L.COOLDOWN_EQUIPMENT_RELIC = "Реликвия / манускрипт"
 else
     L.COOLDOWN_PANEL_ICONS_PER_ROW = "Icons per row: %d"
+    L.COOLDOWN_EQUIPMENT_HEAD_ENCHANT = "Head enchant / engineering tinker"
     L.COOLDOWN_EQUIPMENT_CLOAK_ENCHANT = "Cloak enchant"
+    L.COOLDOWN_EQUIPMENT_HANDS_ENCHANT = "Gloves enchant / engineering tinker"
+    L.COOLDOWN_EQUIPMENT_WAIST_ENCHANT = "Belt engineering tinker"
+    L.COOLDOWN_EQUIPMENT_FEET_ENCHANT = "Boots enchant / engineering tinker"
     L.COOLDOWN_EQUIPMENT_MAINHAND_ENCHANT = "Main-hand enchant"
     L.COOLDOWN_EQUIPMENT_OFFHAND_ENCHANT = "Off-hand enchant"
     L.COOLDOWN_EQUIPMENT_RELIC = "Relic / libram"
