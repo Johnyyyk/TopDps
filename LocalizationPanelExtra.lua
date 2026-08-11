7fce7f09f04fef741cdbd330c8f9eb63d2ca5883
Johnyyyk <36297935+Johnyyyk@users.noreply.github.com>
2026-08-11 19:16:50 +0300

    Добавить тексты расширенных настроек панели

---
+++ LocalizationPanelExtra.lua
@@ -0,0 +1,16 @@
+local addon = TopDps
+local L = addon.L
+
+if GetLocale() == "ruRU" then
+    L.COOLDOWN_PANEL_ICONS_PER_ROW = "Иконок в строке: %d"
+    L.COOLDOWN_EQUIPMENT_CLOAK_ENCHANT = "Чары плаща"
+    L.COOLDOWN_EQUIPMENT_MAINHAND_ENCHANT = "Чары основной руки"
+    L.COOLDOWN_EQUIPMENT_OFFHAND_ENCHANT = "Чары второй руки"
+    L.COOLDOWN_EQUIPMENT_RELIC = "Реликвия / манускрипт"
+else
+    L.COOLDOWN_PANEL_ICONS_PER_ROW = "Icons per row: %d"
+    L.COOLDOWN_EQUIPMENT_CLOAK_ENCHANT = "Cloak enchant"
+    L.COOLDOWN_EQUIPMENT_MAINHAND_ENCHANT = "Main-hand enchant"
+    L.COOLDOWN_EQUIPMENT_OFFHAND_ENCHANT = "Off-hand enchant"
+    L.COOLDOWN_EQUIPMENT_RELIC = "Relic / libram"
+end
