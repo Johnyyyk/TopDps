2a8d67705dffc5f7e8d79558fa0237e13a4fcd96
Johnyyyk <36297935+Johnyyyk@users.noreply.github.com>
2026-08-25 00:12:54 +0300

    Добавить поддержку специализаций друида

---
+++ Cooldowns/DruidDefaults.lua
@@ -0,0 +1,54 @@
+local addon = TopDps
+local Druid = addon.Druid
+
+local function Allowlist(ids)
+    local result = {
+        __allowlist = true,
+    }
+
+    local index
+    for index = 1, #ids do
+        result[ids[index]] = true
+    end
+
+    return result
+end
+
+local function SetDefaults(talentTab, ids)
+    local profile = addon.CooldownRegistry:GetProfile(Druid.CLASS_TOKEN, talentTab)
+    if profile then
+        profile.defaultElementEnabled = Allowlist(ids)
+    end
+end
+
+SetDefaults(Druid.TALENT_TABS.BALANCE, {
+    "moonkinForm",
+    "starfall",
+    "forceOfNature",
+    "barkskin",
+    "innervate",
+    "rebirth",
+    "solarEclipse",
+    "lunarEclipse",
+})
+
+SetDefaults(Druid.TALENT_TABS.FERAL, {
+    "tigersFury",
+    "berserk",
+    "barkskin",
+    "survivalInstincts",
+    "innervate",
+    "rebirth",
+    "clearcasting",
+})
+
+SetDefaults(Druid.TALENT_TABS.RESTORATION, {
+    "treeOfLife",
+    "naturesSwiftness",
+    "swiftmend",
+    "wildGrowth",
+    "barkskin",
+    "innervate",
+    "rebirth",
+    "clearcasting",
+})
